316d153a01007413d8478c572228b41176ad2107
Evgeni Stefanov <102958099+stefanov13@users.noreply.github.com>
2024-07-20 20:15:27 +0300

    Move to client folder

--- src/components/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Footer.module.css"
-
-export default function Footer() {
-    return (
-        <footer>
-            <h4 className={styles.title}>Footer</h4>
-        </footer>
-    );
-};
